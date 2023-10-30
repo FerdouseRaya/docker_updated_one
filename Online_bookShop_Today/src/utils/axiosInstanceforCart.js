214ff17c02039174db1342a80ceeef2a8a45d19d
Jannat Ara Ferdouse Raya <141901088+FerdouseRaya@users.noreply.github.com>
2023-10-30 14:17:07 +0600

    Update axiosInstanceforCart.js

--- Online_bookShop_Today/src/utils/axiosInstanceforCart.js
+++ Online_bookShop_Today/src/utils/axiosInstanceforCart.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstanceforCart = axios.create({
-  baseURL: "http://127.0.0.1:8000/carts",
+  //baseURL: "http://127.0.0.1:8000/carts",
+  baseURL:"http://35.180.242.220:5173/carts",
   timeout: 5000,
 });
 export default axiosInstanceforCart;
