3a50df365893f54c6a312139fa0cfa3827c9dedf
Jannat Ara Ferdouse Raya <141901088+FerdouseRaya@users.noreply.github.com>
2023-10-30 14:04:29 +0600

    Update axiosInstance.js

--- Online_bookShop_Today/src/utils/axiosInstance.js
+++ Online_bookShop_Today/src/utils/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:8000/books",
-  baseURL: "http://localhost:8000/books",
+  // baseURL: "http://localhost:8000/books",
+  baseURL:"http://35.180.242.220/books",
   timeout: 5000,
 });
 export default axiosInstance;
