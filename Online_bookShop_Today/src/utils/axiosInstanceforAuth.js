257599653b8b14eb85b5dafda5af925b4e0cdab4
Jannat Ara Ferdouse Raya <141901088+FerdouseRaya@users.noreply.github.com>
2023-10-30 14:16:43 +0600

    Update axiosInstanceforAuth.js

--- Online_bookShop_Today/src/utils/axiosInstanceforAuth.js
+++ Online_bookShop_Today/src/utils/axiosInstanceforAuth.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstanceforAuth = axios.create({
   // baseURL: "http://127.0.0.1:8000/auth",
-  baseURL: "http://localhost:8000/auth",
+ //baseURL: "http://localhost:8000/auth",
+  baseURL:"http://35.180.242.220:5173/auth",
   timeout: 5000,
 });
 axiosInstanceforAuth.interceptors.request.use((config) => {
