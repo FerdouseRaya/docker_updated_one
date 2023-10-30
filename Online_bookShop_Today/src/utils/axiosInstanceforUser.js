ad38e0f41cad4cf791f8fbc56c65e1a617a35bee
Jannat Ara Ferdouse Raya <141901088+FerdouseRaya@users.noreply.github.com>
2023-10-30 14:20:40 +0600

    Update axiosInstanceforUser.js

--- Online_bookShop_Today/src/utils/axiosInstanceforUser.js
+++ Online_bookShop_Today/src/utils/axiosInstanceforUser.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstanceforUser = axios.create({
-  baseURL: "http://127.0.0.1:8000/users",
+  //baseURL: "http://127.0.0.1:8000/users",
+  baseURL:"http://35.180.242.220:5173/users",
   timeout: 5000,
 });
 export default axiosInstanceforUser;
